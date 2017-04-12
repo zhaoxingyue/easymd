9b68ce6c0d3ef3c5ca82842fe6eb94df87810662
zhaoxingyue <974445496@qq.com>
2017-04-12 11:49:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## easymd
+
+[![Build Status](https://travis-ci.org/zhaoxingyue/easymd.svg?branch=master)](https://travis-ci.org/zhaoxingyue/easymd)
---
+++ test/test.js
@@ -0,0 +1,55 @@
+'use strict'
+
+import test from 'ava'
+import shelljs from 'shelljs'
+import Promise from 'bluebird'
+import cp from 'child_process'
+import fs from 'fs'
+Promise.promisifyAll(cp)
+
+test('Test open markdown file', async t => {
+  try {
+    const cmd = 'easymd -b README.md'
+    const _root = await cp.execAsync('cd ~ && pwd')
+    const result = await cp.execAsync(cmd)
+    const viewsPath = `${_root.split('\n')[0]}/.easymd/views/README.html`
+
+    const content = fs.readFileSync(viewsPath).toString()
+    t.is(content.indexOf('<!DOCTYPE html>'), 0)
+  } catch (e) {
+    t.fail()
+  }
+})
+
+test('Test create html file', async t => {
+  try {
+    const cmd = 'easymd -f README.md'
+    const result = await cp.execAsync(cmd)
+
+    const viewsPath = process.cwd() + '/README.html'
+    const content = fs.readFileSync(viewsPath).toString()
+    t.is(content.indexOf('<!DOCTYPE html>'), 0)
+  } catch (e) {
+    t.fail()
+  }
+})
+
+test('Test other option', async t => {
+  const cmd = 'easymd -c'
+  try {
+    const result = await cp.execAsync(cmd)
+  } catch (err) {
+    t.is(err.code, 1)
+    t.is(err.message.indexOf('error: unknown option `-c\''), 40)
+  }
+})
+
+test('Test no option', async t => {
+  const cmd = 'easymd -b'
+  try {
+    const result = await cp.execAsync(cmd)
+  } catch (err) {
+    t.is(err.code, 1)
+    t.is(err.message.indexOf('error: option `-b, --browser <file>\' argument missing\n\n'), 40)
+  }
+})
